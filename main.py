36c37c774433338c7c5af54e6bfef731a79a5f6e
Shams <shamssaaticho9@gmail.com>
2025-05-06 18:53:07 +1000

    added MainWindow class

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 import os
 import json
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
+import packages.json_util
 
 import sys
 
 DATA_JSON = os.path.join(os.getcwd(), 'data.json')
 
-def load_data():
-    if os.path.exists(DATA_JSON):
-        with open('DATA_JSON', 'r') as f:
-            return json.load(f)
-    else:
-        return []
-
-def save_data(data):
-    with open(DATA_JSON, 'w') as f:
-        json.dump(data, f, indent=2)
-
 app = QApplication(sys.argv)
 
-window = QWidget()
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('File System Tagger')
+        button = QPushButton('Start')
+
+        self.setCentralWidget(button)
+
+window = MainWindow()
 window.show()
 
 app.exec()
