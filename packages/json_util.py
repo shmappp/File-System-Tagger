6bb75c99eee5b607d2dffa463198390f249d12b9
Shams <shamssaaticho9@gmail.com>
2025-05-06 18:52:59 +1000

    moved json utils

---
+++ packages/json_util.py
@@ -0,0 +1,13 @@
+import os
+import json
+
+def load_data(json_path):
+    if os.path.exists(json_path):
+        with open(json_path, 'r') as f:
+            return json.load(f)
+    else:
+        return []
+
+def save_data(data, json_path):
+    with open(json_path, 'w') as f:
+        json.dump(data, f, indent=2)
